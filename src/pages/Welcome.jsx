4471649a66fabddffa707f57fbbe61913581ed36
ktrnatra <ynatura03@gmail.com>
2026-07-06 19:09:53 +0800

    Update Welcome.jsx

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -8,22 +8,22 @@ export default function Welcome() {
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       <style>{"* { box-sizing: border-box; }"}</style>
       <div className="flex w-[393px] h-[852px] flex-col justify-between items-center pt-24 pb-12 bg-[#fbff12] px-8 overflow-clip">
         <div className="flex relative w-[320px] h-[359px] flex-col shrink-0 justify-center items-center gap-x-[34px] gap-y-px">
           <div className="relative w-[217px] h-[177px] shrink-0 bg-[url('https://cdn.wonder.so/images/019f269b-6b46-777f-b695-785e0fb4f427/9832b141a065c3dfafea3c9738990b0f1d62105960696f7e9652a5e8768c7446.png')] bg-cover bg-no-repeat overflow-clip"></div>
-          <span className="relative w-[373px] h-24 shrink-0 text-black font-['Inter'] text-[70px] text-center break-words [display:inline-block] [overflow-wrap:break-word]">PAWHUB</span>
+          <span className="relative w-[373px] h-24 shrink-0 text-black font-['Rubik'] text-[70px] text-center break-words [display:inline-block] [overflow-wrap:break-word]">PAWHUB</span>
         </div>
         <div className="flex w-full flex-col items-center mt-10 mb-0 mx-0 gap-9">
           <div className="flex w-[250px] h-[68px] shrink-0 justify-center items-center bg-[#0c0e09] rounded-[34px]">
-            <div className="text-[#fbff12] font-['Inter'] text-xl font-bold leading-normal tracking-[0.5px] [display:inline-block] [overflow-wrap:normal]">Get Started</div>
+            <div className="text-[#fbff12] font-['Rubik'] text-xl font-bold leading-normal tracking-[0.5px] [display:inline-block] [overflow-wrap:normal]">Get Started</div>
           </div>
           <div className="flex h-fit justify-center items-center gap-2">
             <div className="size-3.5 flex flex-col text-[#0c0e09]">
               <svg stroke="rgb(12, 14, 9)" fill="rgb(12, 14, 9)" strokeWidth="0" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" color="rgb(12, 14, 9)"><path d="M256 224c-79.41 0-192 122.76-192 200.25 0 34.9 26.81 55.75 71.74 55.75 48.84 0 81.09-25.08 120.26-25.08 39.51 0 71.85 25.08 120.26 25.08 44.93 0 71.74-20.85 71.74-55.75C448 346.76 335.41 224 256 224zm-147.28-12.61c-10.4-34.65-42.44-57.09-71.56-50.13-29.12 6.96-44.29 40.69-33.89 75.34 10.4 34.65 42.44 57.09 71.56 50.13 29.12-6.96 44.29-40.69 33.89-75.34zm84.72-20.78c30.94-8.14 46.42-49.94 34.58-93.36s-46.52-72.01-77.46-63.87-46.42 49.94-34.58 93.36c11.84 43.42 46.53 72.02 77.46 63.87zm281.39-29.34c-29.12-6.96-61.15 15.48-71.56 50.13-10.4 34.65 4.77 68.38 33.89 75.34 29.12 6.96 61.15-15.48 71.56-50.13 10.4-34.65-4.77-68.38-33.89-75.34zm-156.27 29.34c30.94 8.14 65.62-20.45 77.46-63.87 11.84-43.42-3.64-85.21-34.58-93.36s-65.62 20.45-77.46 63.87c-11.84 43.42 3.64 85.22 34.58 93.36z" color="rgb(12, 14, 9)" fill="rgb(12, 14, 9)" stroke="rgb(12, 14, 9)" strokeWidth="0px"></path></svg>
             </div>
-            <div className="text-[#0c0e09] font-['Inter'] text-sm font-semibold leading-normal tracking-[0.35px] [display:inline-block] [overflow-wrap:normal]">Made for pet lovers</div>
+            <div className="text-[#0c0e09] font-['Rubik'] text-sm font-semibold leading-normal tracking-[0.35px] [display:inline-block] [overflow-wrap:normal]">Made for pet lovers</div>
           </div>
         </div>
         <div className="relative w-[47px] h-[35px] shrink-0 overflow-clip"></div>
