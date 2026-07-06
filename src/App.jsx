57a85ebd8dc3f6b30af8e20fab58f90715324aea
ktrnatra <ynatura03@gmail.com>
2026-07-07 04:24:32 +0800

    Link Coco pet profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import PetProfileCoco from './pages/PetProfileCoco.jsx';
 import Welcome from './pages/Welcome.jsx';
 import CreateAccount from './pages/CreateAccount.jsx';
 import Login from './pages/Login.jsx';
@@ -21,6 +22,7 @@ const routes = [
   { id: 'MyPets', label: "My pets page", Component: MyPets },
   { id: 'PetSheet', label: "pet sheet", Component: PetSheet },
   { id: 'PetProfile', label: "pet profile", Component: PetProfile },
+  { id: 'PetProfileCoco', label: 'Coco pet profile', Component: PetProfileCoco },
   { id: 'Community', label: "Community page", Component: Community },
   { id: 'CreatePost', label: "Create post page", Component: CreatePost },
   { id: 'Profile', label: "user profile page", Component: Profile },
@@ -76,7 +78,7 @@ function DemoHotspots({ page, setPage }) {
           <Hotspot to="MenuOverlay" setPage={setPage} label="Open menu" className="left-[20px] top-[20px] w-[64px] h-[64px]" />
           <Hotspot to="MyPets" setPage={setPage} label="View all pets" className="left-[280px] top-[595px] w-[95px] h-[40px]" />
           <Hotspot to="MyPets" setPage={setPage} label="Open Mochi" className="left-[20px] top-[635px] w-[165px] h-[150px]" />
-          <Hotspot to="MyPets" setPage={setPage} label="Open Coco" className="left-[205px] top-[635px] w-[165px] h-[150px]" />
+          <Hotspot to="PetProfileCoco" setPage={setPage} label="Open Coco" className="left-[205px] top-[635px] w-[165px] h-[150px]" />
           {navHotspots(setPage)}
         </>
       );
