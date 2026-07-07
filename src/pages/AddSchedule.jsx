c0657274f786e18906dda657797df0d43df936e8
ktrnatra <ynatura03@gmail.com>
2026-07-07 08:24:08 +0800

    Create AddSchedule.jsx

---
+++ src/pages/AddSchedule.jsx
@@ -0,0 +1,146 @@
+/**
+ * Made with Wonder
+ * https://app.wonder.so/yna-tura/files/019f269b-6b46-777f-b695-785e0fb4f427/branches/main/pages/019f269b-6b48-73f4-9340-064d58b68665?nodeId=019f398b-750a-773e-affa-41a048e5ed11
+ * Jul 7, 2026
+ */
+export default function AddSchedule () {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+      <style>{"* { box-sizing: border-box; }"}</style>
+      <div className="flex w-[393px] h-[852px] flex-col bg-[#0c0e09] overflow-clip">
+        <div className="size-full inset-0 flex absolute flex-col overflow-clip">
+          <div className="size-full inset-0 flex absolute flex-col shrink-0 bg-[url('https://cdn.wonder.so/images/019f269b-6b46-777f-b695-785e0fb4f427/88085cb88b44c04228faa2fa5c052b8d735476b71c2f33c3d9aac2997e2dd7a9.jpg')] bg-cover bg-no-repeat"></div>
+          <div className="flex absolute h-[380px] flex-col left-0 right-0 bottom-0 bg-[linear-gradient(0deg,_rgba(12,_14,_9,_0.6)_0%,_rgba(0,_0,_0,_0)_100%)]"></div>
+        </div>
+        <div className="flex absolute justify-between items-center z-[10] left-0 top-4 right-0 px-4 py-0">
+          <div className="size-[42px] flex justify-center items-center bg-[#0c0e098c] border-t border-b border-l border-r backdrop-blur-md rounded-full border-[#ffffff1a]">
+            <div className="size-5 flex flex-col text-[#fbff12]">
+              <div className="size-5 flex flex-col">
+                <svg stroke="rgb(251, 255, 18)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgb(251, 255, 18)"><line x1="19" y1="12" x2="5" y2="12" color="rgb(251, 255, 18)" fill="none" stroke="rgb(251, 255, 18)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><polyline points="12 19 5 12 12 5" color="rgb(251, 255, 18)" fill="none" stroke="rgb(251, 255, 18)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></polyline></svg>
+              </div>
+            </div>
+          </div>
+          <div className="flex h-[38px] justify-center items-center bg-[#fbff12] shadow-[0px_4px_14px_rgba(12,14,9,0.25)] rounded-full px-4 py-0">
+            <div className="text-[#0c0e09] font-['Inter'] text-[13px] font-bold leading-normal [display:inline-block] [overflow-wrap:normal]">Edit Profile</div>
+          </div>
+        </div>
+        <div className="flex absolute h-fit flex-col z-[10] left-3.5 right-3.5 bottom-5 bg-[#ffffffd9] border-t border-b border-l border-r shadow-[0px_16px_40px_rgba(12,14,9,0.28)] backdrop-blur-xl rounded-[28px] px-[22px] py-6 gap-[18px] border-[#ffffff99]">
+          <div className="flex flex-col gap-[3px]">
+            <div className="text-[#0c0e09] font-['Inter'] text-[28px] font-extrabold leading-normal tracking-[-0.7px] [display:inline-block] [overflow-wrap:normal]">Mochi</div>
+            <div className="text-[#6b6e66] font-['Inter'] text-[13px] font-medium leading-normal [display:inline-block] [overflow-wrap:normal]">Pet ID: PHB-2024-00125</div>
+          </div>
+          <div className="flex flex-wrap gap-y-3.5">
+            <div className="flex w-1/2 flex-col gap-0.5">
+              <div className="text-[#9a9d94] font-['Inter'] text-xs font-medium leading-normal tracking-[0.3px] uppercase [display:inline-block] [overflow-wrap:normal]">Species</div>
+              <div className="text-[#0c0e09] font-['Inter'] text-[15px] font-bold leading-normal [display:inline-block] [overflow-wrap:normal]">Cat</div>
+            </div>
+            <div className="flex w-1/2 flex-col gap-0.5">
+              <div className="text-[#9a9d94] font-['Inter'] text-xs font-medium leading-normal tracking-[0.3px] uppercase [display:inline-block] [overflow-wrap:normal]">Breed</div>
+              <div className="text-[#0c0e09] font-['Inter'] text-[15px] font-bold leading-normal [display:inline-block] [overflow-wrap:normal]">Siamese</div>
+            </div>
+            <div className="flex w-1/2 flex-col gap-0.5">
+              <div className="text-[#9a9d94] font-['Inter'] text-xs font-medium leading-normal tracking-[0.3px] uppercase [display:inline-block] [overflow-wrap:normal]">Age</div>
+              <div className="text-[#0c0e09] font-['Inter'] text-[15px] font-bold leading-normal [display:inline-block] [overflow-wrap:normal]">2 years old</div>
+            </div>
+          </div>
+          <div className="flex gap-3">
+            <div className="flex h-[50px] justify-center items-center bg-[#0c0e09] flex-1 rounded-2xl gap-2">
+              <div className="size-[17px] flex flex-col text-[#fbff12]">
+                <div className="size-[17px] flex flex-col">
+                  <svg stroke="rgb(251, 255, 18)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgb(251, 255, 18)"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" color="rgb(251, 255, 18)" fill="none" stroke="rgb(251, 255, 18)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></path><path d="M13.73 21a2 2 0 0 1-3.46 0" color="rgb(251, 255, 18)" fill="none" stroke="rgb(251, 255, 18)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></path></svg>
+                </div>
+              </div>
+              <div className="text-[#fbff12] font-['Inter'] text-sm font-bold leading-normal [display:inline-block] [overflow-wrap:normal]">Add Reminder</div>
+            </div>
+            <div className="flex h-[50px] justify-center items-center bg-[#fbff12] flex-1 rounded-2xl gap-2">
+              <div className="size-[17px] flex flex-col text-[#0c0e09]">
+                <div className="size-[17px] flex flex-col">
+                  <svg stroke="rgb(12, 14, 9)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgb(12, 14, 9)"><rect x="3" y="4" width="18" height="18" rx="2" ry="2" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></rect><line x1="16" y1="2" x2="16" y2="6" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><line x1="8" y1="2" x2="8" y2="6" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><line x1="3" y1="10" x2="21" y2="10" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line></svg>
+                </div>
+              </div>
+              <div className="text-[#0c0e09] font-['Inter'] text-sm font-bold leading-normal [display:inline-block] [overflow-wrap:normal]">Add Schedule</div>
+            </div>
+          </div>
+        </div>
+        <div className="inset-0 flex absolute justify-center items-center z-[50] bg-[#0c0e0999] px-4 py-0">
+          <div className="flex w-[345px] h-fit flex-col bg-white shadow-[0px_20px_60px_rgba(12,14,9,0.35)] rounded-3xl p-5 gap-4">
+            <div className="flex w-full h-fit items-center gap-3">
+              <div className="size-9 flex shrink-0 justify-center items-center bg-[#fbff12] rounded-full">
+                <div className="size-[18px] flex flex-col text-[#0c0e09]">
+                  <svg stroke="rgb(12, 14, 9)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgb(12, 14, 9)"><rect x="3" y="4" width="18" height="18" rx="2" ry="2" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></rect><line x1="16" y1="2" x2="16" y2="6" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><line x1="8" y1="2" x2="8" y2="6" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><line x1="3" y1="10" x2="21" y2="10" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line></svg>
+                </div>
+              </div>
+              <div className="text-[#0c0e09] font-['Inter'] text-lg font-bold leading-tight [display:inline-block] [overflow-wrap:normal] flex-1">Add New Schedule</div>
+              <div className="size-8 flex shrink-0 justify-center items-center bg-[#f5f5f5] rounded-full">
+                <div className="size-[18px] flex flex-col text-[#0c0e09]">
+                  <svg stroke="rgb(12, 14, 9)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgb(12, 14, 9)"><line x1="18" y1="6" x2="6" y2="18" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><line x1="6" y1="6" x2="18" y2="18" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line></svg>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full h-fit flex-col gap-1.5">
+              <div className="text-[#0c0e0999] font-['Inter'] text-xs font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">Pet</div>
+              <div className="flex w-full h-fit justify-between items-center bg-[#f5f5f5] rounded-[14px] px-4 py-3">
+                <div className="text-[#0c0e09] font-['Inter'] text-sm font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">Mochi</div>
+                <div className="size-[18px] flex flex-col text-[#0c0e0999]">
+                  <svg stroke="rgba(12, 14, 9, 0.6)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgba(12, 14, 9, 0.6)"><polyline points="6 9 12 15 18 9" color="rgba(12, 14, 9, 0.6)" fill="none" stroke="rgba(12, 14, 9, 0.6)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></polyline></svg>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full h-fit flex-col gap-1.5">
+              <div className="text-[#0c0e0999] font-['Inter'] text-xs font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">Title *</div>
+              <div className="flex w-full h-fit items-center bg-[#f5f5f5] rounded-[14px] px-4 py-3">
+                <div className="text-[#0c0e09] font-['Inter'] text-sm leading-none [display:inline-block] [overflow-wrap:normal]">Grooming appointment</div>
+              </div>
+            </div>
+            <div className="flex w-full h-fit flex-col gap-1.5">
+              <div className="flex w-full h-16 items-start bg-[#f5f5f5] rounded-[14px] px-4 py-3">
+                <div className="text-[#0c0e0966] font-['Inter'] text-sm leading-snug [display:inline-block] [overflow-wrap:normal]">Description</div>
+              </div>
+            </div>
+            <div className="flex w-full h-fit flex-col gap-1.5">
+              <div className="text-[#0c0e0999] font-['Inter'] text-xs font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">Type</div>
+              <div className="flex w-full h-fit justify-between items-center bg-[#f5f5f5] rounded-[14px] px-4 py-3">
+                <div className="text-[#0c0e09] font-['Inter'] text-sm font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">Vet Visit</div>
+                <div className="size-[18px] flex flex-col text-[#0c0e0999]">
+                  <svg stroke="rgba(12, 14, 9, 0.6)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgba(12, 14, 9, 0.6)"><polyline points="6 9 12 15 18 9" color="rgba(12, 14, 9, 0.6)" fill="none" stroke="rgba(12, 14, 9, 0.6)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></polyline></svg>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full h-fit items-center bg-[#f5f5f5] rounded-[14px] px-4 py-3 gap-3">
+              <div className="size-[18px] flex flex-col shrink-0 text-[#0c0e09]">
+                <svg stroke="rgb(12, 14, 9)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgb(12, 14, 9)"><rect x="3" y="4" width="18" height="18" rx="2" ry="2" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></rect><line x1="16" y1="2" x2="16" y2="6" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><line x1="8" y1="2" x2="8" y2="6" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line><line x1="3" y1="10" x2="21" y2="10" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></line></svg>
+              </div>
+              <div className="text-[#0c0e0999] font-['Inter'] text-[13px] font-semibold leading-none [display:inline-block] [overflow-wrap:normal] flex-1">Date</div>
+              <div className="text-[#0c0e09] font-['Inter'] text-sm font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">Jul 8, 2026</div>
+              <div className="size-[18px] flex flex-col shrink-0 text-[#0c0e0999]">
+                <svg stroke="rgba(12, 14, 9, 0.6)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgba(12, 14, 9, 0.6)"><polyline points="9 18 15 12 9 6" color="rgba(12, 14, 9, 0.6)" fill="none" stroke="rgba(12, 14, 9, 0.6)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></polyline></svg>
+              </div>
+            </div>
+            <div className="flex w-full h-fit items-center bg-[#f5f5f5] rounded-[14px] px-4 py-3 gap-3">
+              <div className="size-[18px] flex flex-col shrink-0 text-[#0c0e09]">
+                <svg stroke="rgb(12, 14, 9)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgb(12, 14, 9)"><circle cx="12" cy="12" r="10" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></circle><polyline points="12 6 12 12 16 14" color="rgb(12, 14, 9)" fill="none" stroke="rgb(12, 14, 9)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></polyline></svg>
+              </div>
+              <div className="text-[#0c0e0999] font-['Inter'] text-[13px] font-semibold leading-none [display:inline-block] [overflow-wrap:normal] flex-1">Time</div>
+              <div className="text-[#0c0e09] font-['Inter'] text-sm font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">10:00 AM</div>
+              <div className="size-[18px] flex flex-col shrink-0 text-[#0c0e0999]">
+                <svg stroke="rgba(12, 14, 9, 0.6)" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg" color="rgba(12, 14, 9, 0.6)"><polyline points="9 18 15 12 9 6" color="rgba(12, 14, 9, 0.6)" fill="none" stroke="rgba(12, 14, 9, 0.6)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2px"></polyline></svg>
+              </div>
+            </div>
+            <div className="text-[#0c0e0999] font-['Inter'] text-xs leading-snug [display:inline-block] [overflow-wrap:normal]">You will receive a notification 1 hour before the scheduled time.</div>
+            <div className="flex w-full h-fit justify-between items-center pt-1 pb-0 px-0">
+              <div className="flex h-fit items-center px-4 py-2.5">
+                <div className="text-[#0c0e0999] font-['Inter'] text-[15px] font-semibold leading-none [display:inline-block] [overflow-wrap:normal]">Cancel</div>
+              </div>
+              <div className="flex h-fit items-center bg-[#0c0e09] rounded-full px-8 py-3">
+                <div className="text-[#fbff12] font-['Inter'] text-[15px] font-bold leading-none [display:inline-block] [overflow-wrap:normal]">Add</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
